74ba1f57de18ab0f579e6f7bcf29a71e485a0eef
patrickpoole <patrickcpoole@gmail.com>
2022-11-19 15:07:47 -0700

    added framer motion to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,35 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons';
+import { motion } from 'framer-motion'
+
 type Props = {}
 
 function Header({}: Props) {
   return (
-    <header className="sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20">
-      <div className="flex flex-row items-center">
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20">
+      <motion.div 
+      initial={{
+        x:-500,
+        opacity: 0,
+        scale:0.5
+      }}
+      animate={{
+        x:0,
+        opacity: 1,
+        scale: 1
+      }}
+      transition={{
+        duration:1.5
+      }}
+      className='flex flex-row items-center'>
         {/* Social Icons */}
         <SocialIcon 
-        url="https://www.instagram.com/portra_pat/?hl=en" 
+        url='https://www.instagram.com/portra_pat/?hl=en' 
+        fgColor='gray' 
+        bgColor='transparent'  
+        />
+        <SocialIcon 
+        url='https://www.instagram.com/portra_pat/?hl=en' 
         fgColor="gray" 
         bgColor="transparent"  
         />
@@ -17,13 +38,22 @@ function Header({}: Props) {
         fgColor="gray" 
         bgColor="transparent"  
         />
-        <SocialIcon 
-        url="https://www.instagram.com/portra_pat/?hl=en" 
-        fgColor="gray" 
-        bgColor="transparent"  
-        />
-      </div>
-      <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+      </motion.div>
+      <motion.div 
+      initial={{
+        x:500,
+        opacity: 0,
+        scale:0.5
+      }}
+      animate={{
+        x:0,
+        opacity: 1,
+        scale: 1
+      }}
+      transition={{
+        duration:1.5
+      }}
+      className='flex flex-row items-center text-gray-300 cursor-pointer'>
       <SocialIcon
         className="cursor-pointer"
         network="email"
@@ -31,7 +61,7 @@ function Header({}: Props) {
         bgColor="transparent"  
         />
         <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get In Touch</p>
-      </div>
+      </motion.div>
       </header>
   )
 }
