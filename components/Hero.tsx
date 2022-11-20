480cfd561023a279ca72976aac60a58e88c37403
patrickpoole <patrickcpoole@gmail.com>
2022-11-19 18:06:28 -0700

    finished hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,50 @@
+import Link from 'next/link';
+import React from 'react';
+import { Cursor, useTypewriter } from 'react-simple-typewriter';
+import BackgroundCircles from './BackgroundCircles';
+
+type Props = {};
+
+function Hero({}: Props) {
+	const [text, count] = useTypewriter({
+		words: [
+			"Hi! My Name's Patrick",
+			'I love film photography',
+			'And world of warcraft lmao',
+		],
+		loop: true,
+		delaySpeed: 2000,
+	});
+	return (
+		<div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
+			<BackgroundCircles />
+      <img 
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src="https://cdn.quasar.dev/img/avatar1.jpg" alt="avatar photo" />
+      <div className="z-20">
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Software Engineer</h2>
+        
+			<h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+				<span className='mr-3'>{text}</span>
+				<Cursor cursorColor="#F7Ab0A" />
+			</h1>
+      <div className='pt-5'>
+        <Link href="#about">
+        <button className='heroButton'>About</button>
+        </Link>
+        <Link href="#experience">
+        <button className='heroButton'>Experience</button>
+        </Link>
+        <Link href="#skills">
+        <button className='heroButton'>Skills</button>
+        </Link>
+        <Link href="#projects">
+        <button className='heroButton'>Projects</button>
+        </Link>
+        </div>
+      </div>
+		</div>
+	);
+}
+
+export default Hero;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,23 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[rgb(38,38,38)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       <Head>
-        <title>Patrick's portfolio</title>
+        <title>Patricks portfolio</title>
 
       </Head>
 
       {/*Header */}
       <Header />
       {/* Hero */}
+
+      <section id='header' className='snap-center'>
+        <Hero />
+      </section>
 
       {/* About*/}
 
