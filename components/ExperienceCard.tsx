0c3801a3ae22984b1d1b489e9c7c0fec4968e2f9
patrickpoole <patrickcpoole@gmail.com>
2025-01-20 17:49:09 -0800

    updated liminal logo in experience section

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -6,6 +6,7 @@ import { Experience } from "../typings";
 import { urlFor } from "../sanity";
 type Props = { job: Experience };
 function ExperienceCard({ job }: Props) {
+  console.log("job data", job);
   const orderedTechnologies = job.technologies.sort(
     (a, b) => a.order - b.order
   );
@@ -13,27 +14,42 @@ function ExperienceCard({ job }: Props) {
     <article
       className="mt-2 flex flex-col rounded-md justify-between space-y-4 flex-shrink-0 mx-auto shadow-xl 
       snap-center w-11/12 md:max-w-[700px] xl:h-[625px] text-center
-      dark:bg-card bg-cardLight p-5 pb-12 md:p-10 cursor-pointer overflow-hidden hover:shadow-2xl transition-transform duration-10 "
-      onClick={() => window.open(job.linkToWebsite, "_blank")}
+      dark:bg-card bg-cardLight p-5 pb-12 md:p-10 overflow-hidden hover:shadow-2xl transition-transform duration-10 "
     >
       <div className="flex flex-col justify-start items-center">
         <motion.img
+          onClick={() => window.open(job.linkToWebsite, "_blank")}
           initial={{ y: -100, opacity: 0 }}
           transition={{ duration: 1.2 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true }}
-          className="object-cover object-center"
+          className="object-cover object-center cursor-pointer "
           src={urlFor(job?.companyImage).url()}
           style={
             job.company === "WageUp"
               ? { maxWidth: "200px", maxHeight: "100px", marginTop: "-35px" }
-              : { maxWidth: "200px", maxHeight: "150px", marginBottom: "20px" }
+              : {
+                  width: "75px",
+                  height: "75px",
+                  marginBottom: "5px",
+                  marginTop: "-15px",
+                }
           }
           alt="work experience image"
         />
         <div className="px-0 md:px-2">
-          <h4 className="text-3xl font-light">{job.jobTitle}</h4>
-          <p className="font-bold text-xl mt-2">{job.company}</p>
+          <h4
+            className="text-3xl font-light cursor-pointer "
+            onClick={() => window.open(job.linkToWebsite, "_blank")}
+          >
+            {job.jobTitle}
+          </h4>
+          <p
+            className="font-bold text-xl mt-2 cursor-pointer "
+            onClick={() => window.open(job.linkToWebsite, "_blank")}
+          >
+            {job.company}
+          </p>
           <div className="flex flex-wrap justify-center items-center w-full space-x-2 mt-4">
             {orderedTechnologies.map((technology) => (
               <Image
